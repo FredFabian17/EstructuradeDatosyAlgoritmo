a2c0903d252b344c8bcb7fcdf8bd0ce9c98685e7
FredFabian17 <realword17>
2025-03-28 13:21:41 -0400

    Impuestos 3/28//2025

--- Impuestos.py
+++ Impuestos.py
@@ -1,19 +1,51 @@
 # Vamos a ingresar el ingreso anual y calcular el impuesto a pagar
 # Según la escala anual proporcionada
 
-income = float(input("Introduce el ingreso mensual: ")) * 12  # Convertir ingreso mensual a anual
+income_monthly = float(input("Introduce el ingreso mensual en RD$: "))
+income_annual = income_monthly * 12  # Convertir ingreso mensual a anual
 
-if income <= 416220:
+if income_annual <= 416220:
     tax = 0  # Exento
-elif income <= 624329:
-    tax = (income - 416220) * 0.15
-elif income <= 867123:
-    tax = 31216 + (income - 624329) * 0.20
+elif income_annual <= 624329:
+    tax = (income_annual - 416220) * 0.15
+elif income_annual <= 867123:
+    tax = 31216 + (income_annual - 624329) * 0.20
 else:
-    tax = 79776 + (income - 867123) * 0.25
+    tax = 79776 + (income_annual - 867123) * 0.25
 
-monthly_tax = tax / 24  # Dividir el impuesto entre 24
-annual_tax = monthly_tax * 12  # Calcular el impuesto total anual
+monthly_tax = tax / 12  # Dividir el impuesto anual entre 12 para obtener el mensual
+SFS = income_annual * 0.0304  # Calcular el SFS (3% del ingreso anual)
+AFP = income_annual * 0.0287  # Calcular el AFP (2.87% del ingreso anual)
+monthly_tax2 = income_monthly * 0.0469409  # Calcular el impuesto mensual (4.69409% del ingreso mensual)
+print("-----------------------------------")
+print("Tus ingresos anuales son: RD$", "{:,.2f}".format(income_annual))
+print("-----------------------------------")
+print("Tu pago diario es de: RD$", "{:,.2f}".format(income_monthly / 23.83))  # Suponiendo 23.83 días al mes
+print("-----------------------------------")
+print ("tu descuento de SFS mensual de: RD$", "{:,.2f}".format(SFS / 12))  # Dividir el SFS anual entre 12 para obtener el mensual
+print("lo que hace un total anual de: RD$", "{:,.2f}".format(SFS))
+print("-----------------------------------")
+print("tu descuento de AFP mensual de: RD$", "{:,.2f}".format(AFP / 12))  # Dividir el AFP anual entre 12 para obtener el mensual
+print("lo que hace un total anual de: RD$", "{:,.2f}".format(AFP))
+print("-----------------------------------")   
+print("-----------------------------------")    
+print("Tu pagas un impuesto mensual de: RD$", "{:,.2f}".format(monthly_tax2))  # Dividir el impuesto anual entre 12 para obtener el mensual
+print("-----------------------------------")
+print("-----------------------------------")
+print("te van a pagar una bonificacion por 60 dias de: RD$", "{:,.2f}".format((income_monthly / 23.83) * 60))  # Dividir el impuesto anual entre 12 para obtener el mensual   
+print("-----------------------------------")    
+print("tomando en cuenta que a esto hay que cobrarle el 25% de impuestos")
+print("lo que hace un total de: RD$", "{:,.2f}".format(((income_monthly / 23.83) * 60) * 0.25))  # Dividir el impuesto anual entre 12 para obtener el mensual
+print("Te pagaran un total de: RD$", "{:,.2f}".format(((income_monthly / 23.83) * 60) - (((income_monthly / 23.83) * 60) * 0.25)))  # Dividir el impuesto anual entre 12 para obtener el mensual    
+print("-----------------------------------")
 
-print("El impuesto total anual es: ", round(annual_tax, 2))
-print("El impuesto mensual es: ", round(monthly_tax, 2))
+
+
+
+
+
+#print("El impuesto mensual es: RD$", "{:,.2f}".format(monthly_tax))
+
+
+#print("El impuesto total anual es: RD$", "{:,.2f}".format(tax))
+#print("-----------------------------------")
