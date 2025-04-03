2fb0ee96f507ac447083c0a860d959b8e69c6f68
FredFabian17 <realword17>
2025-04-02 20:03:54 -0400

    funcion

---
+++ FuncReturnt.py
@@ -0,0 +1,29 @@
+def suma (a,b):
+    resultado = a + b
+    return resultado
+def resta (a,b):
+    resultado = a - b
+    return resultado
+def multiplicar (a,b):
+    resultado = a * b
+    return resultado
+def dividir (a,b):
+    resultado = a / b
+    return resultado
+def potencia (a,b):
+    resultado = a ** b
+    return resultado
+def raiz (a,b):
+    resultado = a ** (1/b)
+    return resultado
+
+
+print("suma: ", suma(5,6))
+print("resta: ", resta(5,6))
+print("multiplicar: ", multiplicar(5,6))
+print("dividir: ", dividir(5,6))
+print("potencia: ", potencia(5,6))
+print("raiz: ", raiz(5,6))
+
+print("Gracias por usar la calculadora")
+print("Fin del programa")
